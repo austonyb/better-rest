e79f0503c8a7790553111c1b93bbcbe66d31a24e
austonyb <auston.youngblood@gmail.com>
2023-04-08 10:38:02 -0600

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Auston Youngblood on 4/8/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
